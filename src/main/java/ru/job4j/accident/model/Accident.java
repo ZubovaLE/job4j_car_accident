3d30f534eb1ee33afb792f47770deef38e6e0373
Lyubov <n.lubovj@gmail.com>
2024-02-10 23:59:28 +0300

    Change Spring version, add classes

---
+++ src/main/java/ru/job4j/accident/model/Accident.java
@@ -0,0 +1,16 @@
+package ru.job4j.accident.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Accident {
+    @EqualsAndHashCode.Include
+    private int id;
+    private String name;
+    private String text;
+    private String address;
+}
