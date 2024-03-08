f14c9352c63be0078e29f2e2b04b22e14eaf873e
Lyubov <n.lubovj@gmail.com>
2024-03-08 23:25:40 +0300

    Add scheme, add models

---
+++ src/main/java/ru/job4j/accident/model/Authority.java
@@ -0,0 +1,21 @@
+package ru.job4j.accident.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "authorities")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private String authority;
+}
